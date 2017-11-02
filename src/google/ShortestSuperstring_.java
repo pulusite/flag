5e3db6bcf5a4f9e07ceebd9bbb9b574021c436ca
pulusite <pulusite@126.com>
2017-11-02 15:55:05 +0800

    update

---
+++ src/google/ShortestSuperstring_.java
@@ -0,0 +1,16 @@
+package google;
+
+/**
+ * Created by zhangdong on 2017/11/2.
+ * Given a set of n strings arr[], find the smallest string that contains each string in the given set as substring. We may assume that no string in arr[] is substring of another string.
+
+ * Examples:
+ * Input:  arr[] = {"geeks", "quiz", "for"}
+ *Output: geeksquizfor
+
+ * Input:  arr[] = {"catg", "ctaagt", "gcta", "ttca", "atgcatc"}
+ * Output: gctaagttcatgcatc
+ */
+public class ShortestSuperstring_ {
+
+}
