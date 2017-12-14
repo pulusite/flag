e9ef5765b28ff43f2a1dbdfc7be8a54eae8db6b7
zhangdong8 <zhangdong8@jd.com>
2017-12-14 11:04:48 +0800

    update

--- src/cc150/Cc0101.java
+++ src/cc150/Cc0101.java
@@ -9,7 +9,10 @@ public class Cc0101 {
         
         boolean[] char_set= new boolean[256];
         for (int i = 0; i < str.length(); i++) {
-            
+            if (char_set[str.charAt(i)]){
+                return false;
+            }
+            char_set[str.charAt(i)]=true;
         }
         return true;
     }
