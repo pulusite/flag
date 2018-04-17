583b29e591f98582e2f561e77b692ff180bbd077
pulusite <pulusite@126.com>
2018-04-17 23:46:18 +0800

    delete

--- src/tba/io/FormattedMemoryInput.java
+++ src/tba/io/FormattedMemoryInput.java
@@ -1,25 +1,18 @@
-package tba.io;
+package tba.io;//: io/FormattedMemoryInput.java
+import java.io.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.EOFException;
-
-/**
- * Created by dongzhang on 5/21/17.
- */
 public class FormattedMemoryInput {
-    public static void main(String[] args) throws Exception{
-        try{
-            DataInputStream in = new DataInputStream(
-                    new ByteArrayInputStream(
-                            BufferedReader_.read(
-                                    System.getProperty("user.dir")+"/src/cc/io/FormattedMemoryInput.java").getBytes()));
-            while (true)
-                System.err.print((char)in.readByte());
-        }catch (EOFException e){
-            System.err.println("End of stream");
-        }
-
-
+  public static void main(String[] args)
+  throws IOException {
+    try {
+      DataInputStream in = new DataInputStream(
+        new ByteArrayInputStream(
+         BufferedInputFile.read(
+          "FormattedMemoryInput.java").getBytes()));
+      while(true)
+        System.out.print((char)in.readByte());
+    } catch(EOFException e) {
+      System.err.println("End of stream");
     }
-}
+  }
+} /* (Execute to see output) *///:~
--- src/tba/io/MemoryInput.java
+++ src/tba/io/MemoryInput.java
@@ -1,16 +1,13 @@
-package tba.io;
+package tba.io;//: io/MemoryInput.java
+import java.io.*;
 
-import java.io.StringReader;
-
-/**
- * Created by dongzhang on 5/21/17.
- */
 public class MemoryInput {
-    public static void main(String[] args) throws Exception{
-        StringReader in = new StringReader(BufferedReader_.read(System.getProperty("user.dir")+"/src/cc/io/MemoryInput.java"));
-        int c;
-        while ((c = in.read()) != -1){
-            System.out.print((char)c);
-        }
-    }
-}
+  public static void main(String[] args)
+  throws IOException {
+    StringReader in = new StringReader(
+      BufferedInputFile.read("MemoryInput.java"));
+    int c;
+    while((c = in.read()) != -1)
+      System.out.print((char)c);
+  }
+} /* (Execute to see output) *///:~
