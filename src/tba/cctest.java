311dc17428e69bc34217552e892a838ab664eae4
zhangdong8 <zhangdong8@jd.com>
2018-04-18 10:34:22 +0800

    update

--- src/tba/cctest.java
+++ src/tba/cctest.java
@@ -8,5 +8,6 @@ import java.text.SimpleDateFormat;
 public class cctest {
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat();
+        System.out.println("22");
     }
 }
