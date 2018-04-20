319cf1b7554e3fad303e96d6f903119e4c25c0a0
zhangdong8 <zhangdong8@jd.com>
2018-04-20 18:41:19 +0800

    javassist

--- src/tba/cctest.java
+++ src/tba/cctest.java
@@ -1,6 +1,10 @@
 package tba;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by dongzhang on 4/1/17.
@@ -9,5 +13,7 @@ public class cctest {
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat();
         System.out.println("22");
+        Queue queue=new LinkedList();
+        List list=new ArrayList();
     }
 }
